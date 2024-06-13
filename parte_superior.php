3b66e80990897a2f3ba2e2870c946d021394ee5f
Aaronbenavidescr <aaronbenavidescr@gmail.com>
2024-06-12 21:53:52 -0600

    Commit inicial con todos los archivos del proyecto

---
+++ parte_superior.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="English">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="Proyecto de tercera 1" content="Pagina de prueba">
+    <title>
+        Tercera 1
+    </title>
+
+    <link rel="stylesheet" href="./styles/main.css"
+
+
+
+</head>
+
+<menu>
+
+
+    <div class="menu-izquierda">
+        
+
+
+
+    </div>
+
+
+
+
+</menu>
